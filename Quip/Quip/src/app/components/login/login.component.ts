76403f03dbbdccb53dc9872e6e237a414f3ea0a6
Saleh Shehata <Sal@localhost.localdomain>
2018-02-26 19:37:11 -0500

    Login and register are working.

--- Quip/Quip/src/app/components/login/login.component.ts
+++ Quip/Quip/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,18 +9,32 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm;
-  constructor() {}
+  loginForm: FormGroup;
+  
+  constructor (private auth: AuthService,
+               private router: Router) { }
+
   ngOnInit() {
     this.loginForm = new FormGroup({
       email: new FormControl("", Validators.compose([
         Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"),
         Validators.required
       ])),
-      pass: new FormControl("", Validators.compose([
+      password: new FormControl("", Validators.compose([
         Validators.required,
         Validators.pattern("(?=^.{6,255}$)((?=.*\d)(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*\d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*"),
       ]))
     })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.auth.login({
+        username: this.loginForm.controls['email'].value,
+        password: this.loginForm.controls['password'].value
+      }).subscribe(
+        _ => this.router.navigate(['profile']),
+        _ => this.loginForm.reset());
+    }
   }
 }
