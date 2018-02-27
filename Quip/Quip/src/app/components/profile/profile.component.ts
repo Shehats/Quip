31d6572725d674aa6880a8817916f018c6624953
Matheu DeWitt <msdewitt@bsu.edu>
2018-02-27 14:00:18 -0500

    Add profile route navigation check that reroutes if not logged in.

--- Quip/Quip/src/app/components/profile/profile.component.ts
+++ Quip/Quip/src/app/components/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { ActionsService } from '../../services/http/actions.service';
+import { Backend } from '../../Interfaces/Backend';
+import { Profile } from '../../models/Profile';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  backend: Backend = new Backend();
+  profile: Profile;
+  constructor(private actions: ActionsService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.actions.fetch(this.backend.profile).subscribe(
+      ()=> console.log("Insert actions on successful profile navigation here."),
+      ()=>this.router.navigate(['login']));
   }
 
 }
