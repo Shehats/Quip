767110e70a00830a9bcd66cbda0d03c19fd1dc44
unknown <vaiktorg1@gmail.com>
2018-02-27 09:17:33 -0500

    Modified routes

--- Quip/Quip/src/app/modules/auth/auth-routing.module.ts
+++ Quip/Quip/src/app/modules/auth/auth-routing.module.ts
@@ -5,10 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../../components/login/login.component';
 import { RegisterComponent } from '../../components/register/register.component';
 import { ForgotPasswordComponent } from '../../components/forgot-password/forgot-password.component';
-import { SplashComponent } from '../../components/splash/splash.component'
+import { ProfileComponent } from '../../components/profile/profile.component'
 
 const routes: Routes = [
-  { path: '', component: SplashComponent },
+  { path: '', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent }
