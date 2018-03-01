e8d4b822bc80afb31ebf647587b3e6c8327a3ea8
Tanner McDonald <Tanner.McDonald@outlook.com>
2018-03-01 14:55:02 -0500

    Yo lemme get them headers fam

--- Quip/Quip/src/app/components/post/post.component.ts
+++ Quip/Quip/src/app/components/post/post.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from 'app/models/Post';
- import { Account } from 'app/models/Account';
+import { Account } from 'app/models/Account';
 // import { Comment } from 'app/models/Comment';
-// import { Profile } from 'app/models/Profile';
-//import { ActionsService } from 'app/services/http/actions.service';
-// import { Backend } from 'app/Interfaces/Backend';
+import { Profile } from 'app/models/Profile';
+import { ActionsService } from 'app/services/http/actions.service';
+import { Backend } from 'app/Interfaces/Backend';
 
 
 @Component({
@@ -16,18 +16,20 @@ export class PostComponent implements OnInit {
   @Input() post: Post;
   @Input() acc: Account;
 
-  constructor(/*private actions: ActionsService*/) { }
+  constructor(private actions: ActionsService, private backend: Backend) { }
   
-  // createPost(posting: Post) {
-  //   this.post = posting;
-  //   this.actions.fetchById(this.backend.profile, posting.parentId).subscribe(
-  //     profile => this.profile = <Profile>profile,
-  //     _ => console.log("ay bb u want sum fuk?????"),
-  //     () => console.log("we in it boissssss")
-  //   );
-  //   console.log(this.profile);
-  //   this.acc = this.profile.account;
-  // }
+  profile: Profile;
+
+  createPost(posting: Post) {
+    this.post = posting;
+    this.actions.fetchById(this.backend.profile, posting.parentId).subscribe(
+      profile => this.profile = <Profile>profile,
+      _ => console.log("ay bb u want sum fuk?????"),
+      () => console.log("we in it boissssss")
+    );
+    console.log(this.profile);
+    this.acc = this.profile.account;
+  }
 
   ngOnInit() {
     // this.createPost(this.post);
--- Quip/Quip/src/app/components/profile/profile.component.ts
+++ Quip/Quip/src/app/components/profile/profile.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Profile } from 'app/models/Profile';
-import { Post } from 'app/models/Post';
-import { Comment } from 'app/models/Comment';
-import { Account } from 'app/models/Account';
+// import { Profile } from 'app/models/Profile';
+// import { Post } from 'app/models/Post';
+// import { Comment } from 'app/models/Comment';
+// import { Account } from 'app/models/Account';
+import { ActionsService } from 'app/services/http/actions.service';
+import { Backend } from 'app/Interfaces/Backend';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -11,24 +15,30 @@ import { Account } from 'app/models/Account';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private action: ActionsService, private backend: Backend, private actRoute: ActivatedRoute) { }
 
-  newComment = new Comment(235, 12, "bitches ain't shit but hos and tricks", 500, 1);
-  comments = [this.newComment];
-  post = new Post(12, 1234, "Hey bb, you lookin supa fly", 12, 400, this.comments);
-  post2 = new Post(12, 12, "FUCK WITH ME", 12, 400, this.comments);
-  post3 = new Post(12, 4, "AY VICTOR YOU SMELL", 12, 400, this.comments);
-  post4 = new Post(12, 14, "MonkaS", 12, 400, this.comments);
-  post5 = new Post(12, 124, "MonkaBold", 12, 400, this.comments);
-  acc = new Account(12, 'Mattyboi', 'Mat', 'Matterson', 23, 'Mat@Mattyboi.com');
-  account: Account = this.acc;
-  posts: Post[] = [this.post, this.post2, this.post3, this.post4, this.post5];
-  friends: Account[] = [];
-  recommendedFriends: Account[] = [];
 
-  fake: Profile = new Profile(this.acc, this.posts, this.friends, this.recommendedFriends);
+
+  // newComment = new Comment(235, 12, "bitches ain't shit but hos and tricks", 500, 1);
+  // comments = [this.newComment];
+  // post = new Post(12, 1234, "Hey bb, you lookin supa fly", 12, 400, this.comments);
+  // post2 = new Post(12, 12, "FUCK WITH ME", 12, 400, this.comments);
+  // post3 = new Post(12, 4, "AY VICTOR YOU SMELL", 12, 400, this.comments);
+  // post4 = new Post(12, 14, "MonkaS", 12, 400, this.comments);
+  // post5 = new Post(12, 124, "MonkaBold", 12, 400, this.comments);
+  // acc = new Account(12, 'Mattyboi', 'Mat', 'Matterson', 23, 'Mat@Mattyboi.com');
+  // account: Account = this.acc;
+  // posts: Post[] = [this.post, this.post2, this.post3, this.post4, this.post5];
+  // friends: Account[] = [];
+  // recommendedFriends: Account[] = [];
+
+  // fake: Profile = new Profile(this.acc, this.posts, this.friends, this.recommendedFriends);
 
   ngOnInit() {
+    this.action.fetch(this.backend.profile) // Fetching Username
+    .subscribe(
+      () => console.log(this.actRoute) // this.username = this.actRoute
+    )
   }
 
 }
