515236357f6f618d40591cf0c6df42d642de772f
Saleh Shehata <Sal@USF-Gold-Wifi-226-111-96.laptops.usf.edu>
2018-03-02 14:10:20 -0500

    Fix Circular json.

--- Quip/src/main/java/com/evil/scheme/Quip/entities/posts/Post.java
+++ Quip/src/main/java/com/evil/scheme/Quip/entities/posts/Post.java
@@ -98,7 +98,7 @@ public class Post implements Serializable{
         this.dislikes = dislikes;
     }
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "parentPost")
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "parentPost")
     public List<Comment> getComments() {
         return comments;
     }
