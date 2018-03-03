87b4bb250c979e0a9f23bc8f7f48ebc45865fd92
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-03 14:34:22 -0500

    Implement post service.

--- Quip/Quip/src/app/models/Post.ts
+++ Quip/Quip/src/app/models/Post.ts
@@ -11,6 +11,15 @@ export class Post implements Instance{
     likes: Account[];
     mediaUrl: string;
     title: string;
+//Java pojo for reference.
+    // private Long id;
+    // private Account owner;
+    // private String title;
+    // private String description;
+    // private String mediaUrl;
+    // private List<Account> likes;
+    // private List<Account> dislikes;
+    // private List<Comments> comments;
 
     constructor(comments?: Comment[], description?: string, dislikes?: Account[],
                 id?: number, likes?: Account[], mediaUrl?: string, title?: string) {
